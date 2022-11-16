2b4d06ab3a5bda4299ed15f6d01286f3c5862f6b
Your Name <email@example.com>
2022-11-16 13:56:05 +0800

    master 55分支

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,10 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("master分支第一次");
-        System.out.println("master分支第二次+++++++++++++++++");
-        System.out.println("dev分支第一次");
-        System.out.println(55555);
-        System.out.println("master分支第三次");
-        System.out.println("dev分支第二次");
+
     }
 }
