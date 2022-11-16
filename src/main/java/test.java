3d12c59253d59fa0bb72a11b14599838414c897b
Your Name <email@example.com>
2022-11-16 12:42:40 +0800

    first git dev分支

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("master分支第一次");
         System.out.println("master分支第二次+++++++++++++++++");
+        System.out.println("dev分支第一次");
     }
 }
